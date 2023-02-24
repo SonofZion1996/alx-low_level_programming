036a7cf34a927dde801bd0777d5bda06e8b3e2e1
Euloge NeverBrokeAgain <eulogekonanproworld@gmail.com>
2023-02-23 21:24:14 -0800

    Write a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,16 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - prints _putchar
+ * main - prints Holberton
  *
- * Description: prints _putchar
+ * Description: prints Holberton
  *
  * Return: 0 (Success)
  */
 
 int main(void)
 {
-	char text[10] = "_putchar";
+	char text[10] = "Holberton";
 	int i = 0;
 
 	for (i = 0; i < 9; i++)
